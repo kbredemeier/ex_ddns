25453f86289d21d81a8da9df20dc7d9d0c39c733
Kristopher Bredemeier <kb@i22.de>
2018-05-14 21:12:56 +0200

    Initial commit

---
+++ lib/exddns.ex
@@ -0,0 +1,18 @@
+defmodule ExDDNS do
+  @moduledoc """
+  Documentation for ExDDNS.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExDDNS.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exddns_test.exs
@@ -0,0 +1,8 @@
+defmodule ExDDNSTest do
+  use ExUnit.Case
+  doctest ExDDNS
+
+  test "greets the world" do
+    assert ExDDNS.hello() == :world
+  end
+end
